b13a2c4d2ebfa4b28dc84bfdd081dbdc4c5903ae
adel <adeelguitoun@gmail.com>
2023-05-03 20:27:46 +0100

    Use Accrdion component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import Accordion from './components/accordion/Accordion';
 
 import './App.css';
+import AccordionItem from './components/accordion/AccordionItem';
+import AccordionHeader from './components/accordion/AccordionHeader';
+import AccordionBody from './components/accordion/AccordionBody';
 
 function App() {
   return (
     <div className="App">
-      <Accordion />
+      <Accordion>
+        <AccordionItem>
+          <AccordionHeader>This is a header</AccordionHeader>
+          <AccordionBody>This is a Description</AccordionBody>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
