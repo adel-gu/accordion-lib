5d022f5527f6760103bf9b0b4bb4019fbbf98c91
adel <adeelguitoun@gmail.com>
2023-05-04 19:11:41 +0100

    Add custome Icons to accordion header

--- src/components/accordion/AccordionHeader.tsx
+++ src/components/accordion/AccordionHeader.tsx
@@ -1,16 +1,28 @@
-import { FC, useContext } from 'react';
+import React, { ButtonHTMLAttributes, FC, useContext } from 'react';
 import { AccordionContext } from './Accordion';
+import { BiChevronUp, BiChevronDown } from 'react-icons/bi';
 
-interface AccordionHeaderProps {
-  children: string;
+interface AccordionHeaderProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  titleIcon: React.ReactNode;
+  closeIcon: React.ReactNode;
+  openIcon: React.ReactNode;
 }
 
-const AccordionHeader: FC<AccordionHeaderProps> = ({ children }) => {
+const AccordionHeader: FC<AccordionHeaderProps> = ({
+  children,
+  titleIcon,
+  closeIcon,
+  openIcon,
+  ...props
+}) => {
   const { isToggle, handleToggle } = useContext(AccordionContext)!;
   return (
     <div id="Accordion-Header">
       <button onClick={handleToggle}>
-        {children} <span>{isToggle ? '-' : '+'}</span>
+        {children}{' '}
+        {isToggle
+          ? closeIcon || <BiChevronUp />
+          : openIcon || <BiChevronDown />}
       </button>
     </div>
   );
