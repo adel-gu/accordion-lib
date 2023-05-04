756707d210dd8a3b952841df89d9576da66fc559
adel <adeelguitoun@gmail.com>
2023-05-04 17:23:43 +0100

    Import/export all accordion components

---
+++ src/components/accordion/index.tsx
@@ -0,0 +1,6 @@
+import Accordion from './Accordion';
+import AccordionItem from './AccordionItem';
+import AccordionHeader from './AccordionHeader';
+import AccordionBody from './AccordionBody';
+
+export { Accordion, AccordionItem, AccordionHeader, AccordionBody };
