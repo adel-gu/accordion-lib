9cf47fcf0f1f5ed58a3366bb1dd681b4170f6817
adel <adeelguitoun@gmail.com>
2023-05-05 10:25:52 +0100

    Setup Accordion types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,34 @@
-export interface AccordionStatus {
+import { ButtonHTMLAttributes } from 'react';
+
+// ! |====================    Accordion Types   ====================|
+interface AccordionStatus {
   isToggle: boolean;
   handleToggle: () => void;
 }
+
+interface AccordionProps {
+  children: React.ReactNode;
+}
+
+interface AccordionItemProps {
+  children: React.ReactNode;
+}
+
+interface AccordionHeaderProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  titleIcon?: React.ReactNode;
+  closeIcon?: React.ReactNode;
+  openIcon?: React.ReactNode;
+}
+
+interface AccordionBodyProps {
+  children: string;
+}
+// ! |================= End Accordion types ===============|
+
+export type {
+  AccordionStatus,
+  AccordionProps,
+  AccordionItemProps,
+  AccordionBodyProps,
+  AccordionHeaderProps,
+};
