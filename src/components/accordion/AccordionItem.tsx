a1b67d013666a6caea49df7204c1446a6a9a72e1
adel <adeelguitoun@gmail.com>
2023-05-06 19:07:06 +0100

    Add Accordion context and custom classNames

--- src/components/accordion/AccordionItem.tsx
+++ src/components/accordion/AccordionItem.tsx
@@ -1,8 +1,22 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
 import { AccordionItemProps } from '../../types';
+import { accordionClassesOptim } from '../../utils/accordion';
+import { AccordionContext } from '../../contexts/accordion';
+import { useAccordion } from '../../hooks/accordion';
 
-const AccordionItem: FC<AccordionItemProps> = ({ children }) => {
-  return <div id="Accordion-item">{children}</div>;
+const AccordionItem: FC<AccordionItemProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  const value = useAccordion();
+  return (
+    <AccordionContext.Provider value={value}>
+      <div className={accordionClassesOptim('border ', className)} {...props}>
+        {children}
+      </div>
+    </AccordionContext.Provider>
+  );
 };
 
 export default AccordionItem;
