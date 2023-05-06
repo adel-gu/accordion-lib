d2315e7beb3a2749b6960045ca59c3737da60439
adel <adeelguitoun@gmail.com>
2023-05-06 19:06:38 +0100

    Remove Accordion context

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -1,16 +1,8 @@
 import { FC } from 'react';
 import { AccordionProps } from '../../types';
-import { AccordionContext } from '../../contexts/accordion';
-import { useAccordion } from '../../hooks/accordion';
 
 const Accordion: FC<AccordionProps> = ({ children }) => {
-  const value = useAccordion();
-
-  return (
-    <AccordionContext.Provider value={value}>
-      {children}
-    </AccordionContext.Provider>
-  );
+  return <div>{children}</div>;
 };
 
 export default Accordion;
