b6d6155115b761fd440829e47c365b28c1554146
adel <adeelguitoun@gmail.com>
2023-05-07 09:40:31 +0100

    Fix custome class names

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -1,8 +1,16 @@
 import { FC } from 'react';
 import { AccordionProps } from '../../types';
+import { AccordionClasses, accordionClassesOptim } from '../../utils/accordion';
 
-const Accordion: FC<AccordionProps> = ({ children }) => {
-  return <div>{children}</div>;
+const Accordion: FC<AccordionProps> = ({ children, className, ...props }) => {
+  return (
+    <div
+      className={accordionClassesOptim(AccordionClasses({ className }))}
+      {...props}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Accordion;
