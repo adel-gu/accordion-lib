542c0d0071a9fa869191a7b98f2c20624c5a2cac
adel <adeelguitoun@gmail.com>
2024-10-21 13:14:12 +0100

    Setup dark mode

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,3 +1,4 @@
+import { withThemeByClassName } from "@storybook/addon-themes";
 import type { Preview } from '@storybook/react';
 import './style.css';
 
@@ -14,5 +15,15 @@ const preview: Preview = {
     },
   },
 };
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: "light",
+      dark: "dark",
+    },
+    defaultTheme: "light",
+  }),
+];
 
 export default preview;
